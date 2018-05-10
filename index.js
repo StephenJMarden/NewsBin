079baa1928421cde3b63b2840301d8e5854a8caf
Stephen Marden <stephen.j.marden@gmail.com>
2018-05-10 01:53:50 -0400

    final tweaks

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const newsKey = "&apiKey=d5a8be0f6d7e4726a2f58ffd8cf2ab1a";
-const newsURL = "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/";
+const newsURL = "https://newsapi.org/v2/";
 
 onStart();
 
 function onStart() {
     fetchAllTopNews();
+    document.getElementById("search-terms").addEventListener("click", searchWiden)
     document.getElementById("search-button").addEventListener("click", onSearchClick);
     document.getElementById("brand").addEventListener("click", onBrandClick);
 }
@@ -16,12 +17,24 @@ function checkKeypress(e) {
     }
 }
 
-function onSmallSearch() {
-
+function checkKeypressSmall(e) {
+    if(e.keyCode == 13) {
+        onSearchClickSmall();
+        return false;
+    }
 }
 
 function onSearchClick() {
     let terms = document.getElementById("search-terms").value;
+    fetchUserSearch(terms);
+}
+
+function onSearchClickSmall() {
+    console.log('worked');
+    let searchTermsElement = document.getElementById("search-terms-small");
+    let terms = searchTermsElement.value;
+    searchTermsElement.classList.remove("search-terms-expand");
+    document.getElementById("search-button-small").onclick = activateSmallSearch;
     fetchUserSearch(terms);
 }
 
@@ -70,15 +83,27 @@ function displayCards(newsList) {
             `<div class="small-12 medium-6 large-4">
                 <a href="${news.url}"><div class="card">
                     <div class="card-section">
-                        <div class="card-image"><img src="${news.urlToImage}" onerror=imgError(this)></div>
+                        <div class="card-image"><img class="float-center" src="${news.urlToImage || 'no-image.jpg'}" onerror=imgError(this)></div>
                         <h5>${news.title}</h5>
                         <h6>${news.source.name}</h6>
-                        <p>${news.description}</p>
+                        <p>${news.description || ""}</p>
                     </div>
                 </div></a>
             </div>`;
         articleContainer.innerHTML += cardHTML;
     });
+
+    let matchMedia = window.matchMedia("(max-width: 700px)");
+    if(!matchMedia.matches) {
+        setTimeout(equalizeCards, 500);
+    }
+}
+
+function equalizeCards() {
+    let cardSection = Array.from(document.querySelectorAll(".card-section"));
+    let heights = cardSection.map(card => card.clientHeight);
+    let largest = Math.max.apply(Math, heights);
+    cardSection.map(card => card.style.height = largest + "px");
 }
 
 function forceHTTPS(link) {
@@ -94,4 +119,13 @@ function imgError(img) {
     img.onerror = "";
     img.src = "";
     img.style = "display: none";
+}
+
+function searchWiden() {
+    document.getElementById("search-terms").classList.add("widen-search");
+}
+
+function activateSmallSearch() {
+    document.getElementById("search-terms-small").classList.add("search-terms-expand");
+    document.getElementById("search-button-small").onclick = onSearchClickSmall;
 }
